73baabbaca2e3d6db14ba83c5b186122df3d12de
rauzh <rauzhevps@mail.ru>
2024-03-23 23:09:27 +0300

    add manager service bl (без отчета)

--- backend/internal/services/manager_service.go
+++ backend/internal/services/manager_service.go
@@ -1,5 +1,9 @@
 package service
 
+import "cookdroogers/internal/models"
+
 type IManagerService interface {
+	Create(*models.Manager) error
+	Get(uint64) (*models.Manager, error)
 	GetRandomManagerID() (uint64, error)
 }
