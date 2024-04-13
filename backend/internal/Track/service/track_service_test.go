f089675c69ebaea2e6aac0cd1aff8c6c438e05d4
rauzh <rauzhevps@mail.ru>
2024-04-13 21:37:16 +0300

    fix

--- backend/internal/Track/service/track_service_test.go
+++ backend/internal/Track/service/track_service_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	mocks "cookdroogers/internal/repo/mocks"
 	"cookdroogers/models"
+	"github.com/stretchr/testify/mock"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,7 +12,7 @@ import (
 func TestTrackService_Get(t *testing.T) {
 
 	mockTrackRepo := mocks.NewTrackRepo(t)
-	mockTrackRepo.EXPECT().Get(uint64(1234)).Return(&models.Track{
+	mockTrackRepo.EXPECT().Get(mock.AnythingOfType("context.backgroundCtx"), uint64(1234)).Return(&models.Track{
 		TrackID:  1234,
 		Title:    "aa",
 		Duration: 120,
