696213df472f815566d4010d5b778f0d37e98c14
rauzh <rauzhevps@mail.ru>
2024-09-29 23:06:14 +0300

    signreq uc

--- backend/internal/requests/sign_contract/usecase/sign_contract_request_test.go
+++ backend/internal/requests/sign_contract/usecase/sign_contract_request_test.go
@@ -1,372 +1,331 @@
 package usecase
 
 import (
-	"cookdroogers/internal/repo/mocks"
+	repo_mocks "cookdroogers/internal/repo/mocks"
 	"cookdroogers/internal/requests/base"
 	base_errors "cookdroogers/internal/requests/base/errors"
 	broker_mocks "cookdroogers/internal/requests/broker/mocks"
 	"cookdroogers/internal/requests/sign_contract"
+	"cookdroogers/internal/requests/sign_contract/data_builder"
 	sctErrors "cookdroogers/internal/requests/sign_contract/errors"
-	signReqRepoMocks "cookdroogers/internal/requests/sign_contract/repo/mocks"
+	"cookdroogers/internal/requests/sign_contract/repo/mocks"
 	transacMock "cookdroogers/internal/transactor/mocks"
 	cdtime "cookdroogers/pkg/time"
-	"errors"
+	"database/sql"
+	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
 	"github.com/stretchr/testify/mock"
 	"log/slog"
 	"testing"
 )
 
 type _depFields struct {
-	artistRepo *mocks.ArtistRepo
-	userRepo   *mocks.UserRepo
+	artistRepo *repo_mocks.ArtistRepo
+	userRepo   *repo_mocks.UserRepo
 
 	transactor *transacMock.Transactor
 	scBroker   *broker_mocks.IBroker
 
-	signReqRepo *signReqRepoMocks.SignContractRequestRepo
+	signReqRepo *mocks.SignContractRequestRepo
+	logger      *slog.Logger
 }
 
-var dberr = errors.New("db err")
+type SignContractRequestUseCaseSuite struct {
+	suite.Suite
+}
 
-func _newMockSignReqDepFields(t *testing.T) *_depFields {
+func _newMockSignReqDepFields(t provider.T) *_depFields {
+	mockSignReqRepo := mocks.NewSignContractRequestRepo(t)
 
-	transactionMock := transacMock.NewTransactor(t)
+	mockArtistRepo := repo_mocks.NewArtistRepo(t)
+	mockUserRepo := repo_mocks.NewUserRepo(t)
+
+	mockTransactor := transacMock.NewTransactor(t)
+
 	mockBroker := broker_mocks.NewIBroker(t)
 
-	mockArtRepo := mocks.NewArtistRepo(t)
-	mockUserRepo := mocks.NewUserRepo(t)
-
-	mockSignReqRepo := signReqRepoMocks.NewSignContractRequestRepo(t)
-
 	f := &_depFields{
-		artistRepo:  mockArtRepo,
+		artistRepo:  mockArtistRepo,
 		userRepo:    mockUserRepo,
-		transactor:  transactionMock,
+		transactor:  mockTransactor,
 		scBroker:    mockBroker,
 		signReqRepo: mockSignReqRepo,
+		logger:      slog.Default(),
 	}
 
 	return f
 }
 
-func TestSignContractRequestUseCase_Decline(t *testing.T) {
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_DeclineOK(t provider.T) {
+	t.Title("Decline: OK")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
 
-	type args struct {
-		signReq *sign_contract.SignContractRequest
-	}
+	t.WithNewStep("Success", func(sCtx provider.StepCtx) {
 
-	tests := []struct {
-		name string
-		in   *args
-		out  error
+		df := _newMockSignReqDepFields(t)
 
-		dependencies func(*_depFields)
-		assert       func(*testing.T, *_depFields)
-	}{
-		{
-			name: "OK",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.OnApprovalRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 9,
-					},
-					Nickname:    "pink floyd",
-					Description: "",
-				},
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithNickname("pink floyd").
+			WithStatus(base.OnApprovalRequest).
+			Build()
+
+		df.signReqRepo.EXPECT().Update(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
+			Request: base.Request{
+				RequestID: 1,
+				Type:      sign_contract.SignRequest,
+				Status:    base.ClosedRequest,
+				ApplierID: req.ApplierID,
+				ManagerID: req.ManagerID,
+				Date:      req.Date,
 			},
-			out: nil,
-			dependencies: func(df *_depFields) {
+			Nickname:    req.Nickname,
+			Description: base.DescrDeclinedRequest,
+		}).Return(nil).Once()
 
-				df.signReqRepo.EXPECT().Update(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.ClosedRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 9,
-					},
-					Nickname:    "pink floyd",
-					Description: base.DescrDeclinedRequest,
-				}).Return(nil).Once()
-			},
-			assert: func(t *testing.T, df *_depFields) {
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
 
-			},
-		},
-		{
-			name: "InvalidNickName",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.OnApprovalRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 9,
-					},
-					Nickname:    "",
-					Description: "",
-				},
-			},
-			out: sctErrors.ErrNickname,
-			dependencies: func(df *_depFields) {
-			},
-			assert: func(t *testing.T, df *_depFields) {
+		err := signReqUseCase.Decline(req)
 
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-
-			f := _newMockSignReqDepFields(t)
-			if tt.dependencies != nil {
-				tt.dependencies(f)
-			}
-
-			signReqUseCase, err := NewSignContractRequestUseCase(f.userRepo, f.artistRepo, f.transactor, f.scBroker, f.signReqRepo, slog.Default())
-
-			// act
-			err = signReqUseCase.Decline(tt.in.signReq)
-
-			// assert
-			if !errors.Is(err, tt.out) {
-				t.Errorf("got %v, want %v", err, tt.out)
-			}
-			if tt.assert != nil {
-				tt.assert(t, f)
-			}
-		})
-	}
+		sCtx.Assert().NoError(err)
+	})
 }
 
-func TestSignContractRequestUseCase_Accept(t *testing.T) {
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_DeclineInvalidNickName(t provider.T) {
+	t.Title("Decline: InvalidNickName")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
 
-	type args struct {
-		signReq *sign_contract.SignContractRequest
-	}
+	t.WithNewStep("Failure", func(sCtx provider.StepCtx) {
+		df := _newMockSignReqDepFields(t)
 
-	tests := []struct {
-		name string
-		in   *args
-		out  error
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithNickname("").
+			WithStatus(base.OnApprovalRequest).
+			Build()
 
-		dependencies func(*_depFields)
-		assert       func(*testing.T, *_depFields)
-	}{
-		{
-			name: "OK",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.OnApprovalRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 9,
-					},
-					Nickname:    "skibidi",
-					Description: "",
-				},
-			},
-			out: nil,
-			dependencies: func(df *_depFields) {
-				df.transactor.EXPECT().WithinTransaction(mock.AnythingOfType("context.backgroundCtx"),
-					mock.Anything).Return(nil).Once()
-			},
-			assert: func(t *testing.T, df *_depFields) {
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
 
-			},
-		},
-		{
-			name: "InvalidNickName",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.OnApprovalRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 9,
-					},
-					Nickname:    "",
-					Description: "",
-				},
-			},
-			out: sctErrors.ErrNickname,
-			dependencies: func(df *_depFields) {
-			},
-			assert: func(t *testing.T, df *_depFields) {
+		err := signReqUseCase.Decline(req)
 
-			},
-		},
-		{
-			name: "AlreadyClosed",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.ClosedRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 9,
-					},
-					Nickname:    "skibidi",
-					Description: "",
-				},
-			},
-			out: base_errors.ErrAlreadyClosed,
-			dependencies: func(df *_depFields) {
-			},
-			assert: func(t *testing.T, df *_depFields) {
-
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-
-			f := _newMockSignReqDepFields(t)
-			if tt.dependencies != nil {
-				tt.dependencies(f)
-			}
-
-			signReqUseCase, err := NewSignContractRequestUseCase(f.userRepo, f.artistRepo, f.transactor, f.scBroker, f.signReqRepo, slog.Default())
-
-			// act
-			err = signReqUseCase.Accept(tt.in.signReq)
-
-			// assert
-			if !errors.Is(err, tt.out) {
-				t.Errorf("got %v, want %v", err, tt.out)
-			}
-			if tt.assert != nil {
-				tt.assert(t, f)
-			}
-		})
-	}
+		sCtx.Assert().ErrorIs(err, sctErrors.ErrNickname)
+	})
 }
 
-func TestSignContractRequestUseCase_Apply(t *testing.T) {
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_ApplyOK(t provider.T) {
+	t.Title("Apply: OK")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
 
-	type args struct {
-		signReq *sign_contract.SignContractRequest
-	}
+	t.WithNewStep("Success", func(sCtx provider.StepCtx) {
 
-	tests := []struct {
-		name string
-		in   *args
-		out  error
+		df := _newMockSignReqDepFields(t)
 
-		dependencies func(*_depFields)
-		assert       func(*testing.T, *_depFields)
-	}{
-		{
-			name: "OK",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    "",
-						ApplierID: 12,
-						ManagerID: 0,
-					},
-					Nickname:    "skibidi",
-					Description: "",
-				},
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithApplierID(12).
+			WithDescription("").
+			WithNickname("skibidi").
+			Build()
+
+		df.scBroker.EXPECT().SendMessage(mock.Anything).Return(0, 0, nil)
+
+		df.signReqRepo.EXPECT().Create(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
+			Request: base.Request{
+				RequestID: 1,
+				Type:      sign_contract.SignRequest,
+				Status:    base.NewRequest,
+				Date:      cdtime.GetToday(),
+				ApplierID: 12,
+				ManagerID: 8,
 			},
-			out: nil,
-			dependencies: func(df *_depFields) {
+			Nickname:    "skibidi",
+			Description: "",
+		}).Return(nil).Once()
 
-				df.scBroker.EXPECT().SendMessage(mock.Anything).Return(0, 0, nil)
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
 
-				df.signReqRepo.EXPECT().Create(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.NewRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 0,
-					},
-					Nickname:    "skibidi",
-					Description: "",
-				}).Return(nil).Once()
+		err := signReqUseCase.Apply(req)
+
+		sCtx.Assert().NoError(err)
+	})
+}
+
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_ApplyFailure(t provider.T) {
+	t.Title("Apply: Failure")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
+
+	t.WithNewStep("Failure", func(sCtx provider.StepCtx) {
+
+		df := _newMockSignReqDepFields(t)
+
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithApplierID(12).
+			WithDescription("").
+			WithNickname("skibidi").
+			Build()
+
+		df.signReqRepo.EXPECT().Create(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
+			Request: base.Request{
+				RequestID: 1,
+				Type:      sign_contract.SignRequest,
+				Status:    base.NewRequest,
+				Date:      cdtime.GetToday(),
+				ApplierID: 12,
+				ManagerID: 8,
 			},
-			assert: func(t *testing.T, df *_depFields) {
+			Nickname:    "skibidi",
+			Description: "",
+		}).Return(sql.ErrConnDone).Once()
 
-			},
-		},
-		{
-			name: "CantCreate",
-			in: &args{
-				signReq: &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    "",
-						ApplierID: 12,
-						ManagerID: 0,
-					},
-					Nickname:    "skibidi",
-					Description: "",
-				},
-			},
-			out: dberr,
-			dependencies: func(df *_depFields) {
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
 
-				df.signReqRepo.EXPECT().Create(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
-					Request: base.Request{
-						RequestID: 1,
-						Type:      sign_contract.SignRequest,
-						Status:    base.NewRequest,
-						Date:      cdtime.GetToday(),
-						ApplierID: 12,
-						ManagerID: 0,
-					},
-					Nickname:    "skibidi",
-					Description: "",
-				}).Return(dberr).Once()
-			},
-			assert: func(t *testing.T, df *_depFields) {
+		err := signReqUseCase.Apply(req)
 
-			},
-		},
-	}
+		sCtx.Assert().ErrorIs(err, sql.ErrConnDone)
+	})
+}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_AcceptOK(t provider.T) {
+	t.Title("Accept: OK")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
 
-			f := _newMockSignReqDepFields(t)
-			if tt.dependencies != nil {
-				tt.dependencies(f)
-			}
+	t.WithNewStep("Success", func(sCtx provider.StepCtx) {
+		df := _newMockSignReqDepFields(t)
 
-			signReqUseCase, err := NewSignContractRequestUseCase(f.userRepo, f.artistRepo, f.transactor, f.scBroker, f.signReqRepo, slog.Default())
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithApplierID(12).
+			WithManagerID(9).
+			WithStatus(base.OnApprovalRequest).
+			WithNickname("skibidi").
+			Build()
 
-			// act
-			err = signReqUseCase.Apply(tt.in.signReq)
+		df.transactor.EXPECT().WithinTransaction(mock.AnythingOfType("context.backgroundCtx"), mock.Anything).Return(nil).Once()
 
-			// assert
-			if !errors.Is(err, tt.out) {
-				t.Errorf("got %v, want %v", err, tt.out)
-			}
-			if tt.assert != nil {
-				tt.assert(t, f)
-			}
-		})
-	}
+		//df.userRepo.EXPECT().UpdateType(mock.AnythingOfType("context.backgroundCtx"), req.ApplierID, models.ArtistUser).Return(nil).Once()
+
+		//df.artistRepo.EXPECT().Create(mock.AnythingOfType("context.backgroundCtx"), &models.Artist{
+		//	UserID:       req.ApplierID,
+		//	Nickname:     req.Nickname,
+		//	ContractTerm: cdtime.GetEndOfContract(),
+		//	Activity:     true,
+		//	ManagerID:    req.ManagerID,
+		//}).Return(nil).Once()
+
+		//df.signReqRepo.EXPECT().Update(mock.AnythingOfType("context.backgroundCtx"), &sign_contract.SignContractRequest{
+		//	Request: base.Request{
+		//		RequestID: 1,
+		//		Type:      sign_contract.SignRequest,
+		//		Status:    base.ClosedRequest,
+		//		ApplierID: req.ApplierID,
+		//		ManagerID: req.ManagerID,
+		//		Date:      req.Date,
+		//	},
+		//	Nickname:    req.Nickname,
+		//	Description: req.Description,
+		//}).Return(nil).Once()
+
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
+
+		err := signReqUseCase.Accept(req)
+
+		sCtx.Assert().NoError(err)
+	})
+}
+
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_AcceptInvalidNickName(t provider.T) {
+	t.Title("Accept: InvalidNickName")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
+
+	t.WithNewStep("Failure", func(sCtx provider.StepCtx) {
+		df := _newMockSignReqDepFields(t)
+
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithApplierID(12).
+			WithManagerID(9).
+			WithStatus(base.OnApprovalRequest).
+			WithNickname("").
+			Build()
+
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
+
+		err := signReqUseCase.Accept(req)
+
+		sCtx.Assert().ErrorIs(err, sctErrors.ErrNickname)
+	})
+}
+
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_AcceptAlreadyClosed(t provider.T) {
+	t.Title("Accept: AlreadyClosed")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
+
+	t.WithNewStep("Failure", func(sCtx provider.StepCtx) {
+		df := _newMockSignReqDepFields(t)
+
+		req := data_builder.NewSignContractRequestBuilder().
+			WithRequestID(1).
+			WithApplierID(12).
+			WithManagerID(9).
+			WithStatus(base.ClosedRequest).
+			WithNickname("skibidi").
+			Build()
+
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
+
+		err := signReqUseCase.Accept(req)
+
+		sCtx.Assert().ErrorIs(err, base_errors.ErrAlreadyClosed)
+	})
+}
+
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_GetOK(t provider.T) {
+	t.Title("Get: OK")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
+	t.WithNewStep("Success", func(sCtx provider.StepCtx) {
+
+		df := _newMockSignReqDepFields(t)
+		df.signReqRepo.EXPECT().Get(mock.AnythingOfType("context.backgroundCtx"), uint64(1)).
+			Return(data_builder.NewSignContractRequestBuilder().WithRequestID(1).Build(), nil).Once()
+
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
+
+		signreq, err := signReqUseCase.(*SignContractRequestUseCase).Get(uint64(1))
+
+		sCtx.Assert().NoError(err)
+		sCtx.Assert().NotNil(signreq)
+		sCtx.Assert().Equal(data_builder.NewSignContractRequestBuilder().WithRequestID(1).Build(), signreq)
+	})
+}
+
+func (s *SignContractRequestUseCaseSuite) TestSignContractRequestUseCase_GetDbErr(t provider.T) {
+	t.Title("Get: DBerr")
+	t.Tags("SignContractRequestUseCase")
+	t.Parallel()
+	t.WithNewStep("Failure", func(sCtx provider.StepCtx) {
+
+		df := _newMockSignReqDepFields(t)
+		df.signReqRepo.EXPECT().Get(mock.AnythingOfType("context.backgroundCtx"), uint64(1)).
+			Return(nil, sql.ErrConnDone).Once()
+
+		signReqUseCase, _ := NewSignContractRequestUseCase(df.userRepo, df.artistRepo, df.transactor, df.scBroker, df.signReqRepo, df.logger)
+
+		signreq, err := signReqUseCase.(*SignContractRequestUseCase).Get(uint64(1))
+
+		sCtx.Assert().ErrorIs(err, sql.ErrConnDone)
+		sCtx.Assert().Nil(signreq)
+	})
+}
+
+func TestSignContractRequestUseCaseSuiteRunner(t *testing.T) {
+	suite.RunSuite(t, new(SignContractRequestUseCaseSuite))
 }
